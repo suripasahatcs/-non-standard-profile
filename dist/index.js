4802db556ac873ffb3455703f9a0aec142ee9210
suripasahatcs <suripa.saha@tcs.com>
2022-01-06 22:09:14 +0530

    chg

--- dist/index.js
+++ dist/index.js
@@ -10595,7 +10595,7 @@ module.exports = class OrganizationUserActivity {
     const self = this;
 
     const orgUsers = await self.organizationClient.findUsers(org);
-    const activityResults = [];
+    let activityResults = [];
     for(let idx = 0; idx< orgUsers.length; idx++) {
       const repoActivity = await self.organizationClient.findNonstdUsers(orgUsers[idx]['login']);
       activityResults =[...activityResults, repoActivity];
