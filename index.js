e05f61443f0ae746944c76d535900b251ba2437f
suripasahatcs <suripa.saha@tcs.com>
2022-01-08 17:15:16 +0530

    suripa

--- index.js
+++ index.js
@@ -89,7 +89,7 @@ function saveIntermediateData(directory, data) {
   try {
     const file = path.join(directory, 'jsonfinallist.json');
     fs.writeFileSync(file, JSON.stringify(data));
-    core.setOutput('jsonfinallist_json', file);
+    core.setOutput('nonstd_profile_list', file);
   } catch (err) {
     console.error(`Failed to save intermediate data: ${err}`);
   }
