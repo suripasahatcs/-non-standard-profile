a00c074af35da0162458f1df7f2ab8bbc4825c33
suripasahatcs <suripa.saha@tcs.com>
2022-01-11 11:24:41 +0530

    chg

--- src/githublib/OrgsUserActivity.js
+++ src/githublib/OrgsUserActivity.js
@@ -54,11 +54,11 @@ module.exports = class OrganizationUserActivity {
           {
             nonstduserattribute.push('public_repos');
           }
-           let regex = /^[0-9]{6,6}$/
-           let validate_login = regex.test(repoActivity[j]['login']);
-           if((!validate_login)){
-            nonstduserattribute.push('login')
-           }
+          //  let regex = /^[0-9]{6,6}$/
+          //  let validate_login = regex.test(repoActivity[j]['login']);
+          //  if((!validate_login)){
+          //   nonstduserattribute.push('login')
+          //  }
          
           console.log('******non std*******')
           // console.log(nonstduserattribute)
